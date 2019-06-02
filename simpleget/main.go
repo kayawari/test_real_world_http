58040700af7bf58d07efe91b83cc1a9b6bea3336
takayuki.kayawari <kayamelo@gmail.com>
2019-06-02 12:48:08 +0900

    クエリ付きのGETメソッドを追加

--- simpleget/main.go
+++ simpleget/main.go
@@ -4,9 +4,11 @@ import (
 	"net/http"
 	"io/ioutil"
 	"log"
+	"net/url"
 )
 
-func main () {
+func simpleGet() {
+	// 単純なget method
 	resp, err := http.Get("http://localhost:18888")
 	if err != nil {
 		panic(err)
@@ -21,4 +23,19 @@ func main () {
 	log.Println("headers:", resp.Header)
 	log.Println("body:", string(body))
 	log.Println("Content-Length", resp.Header.Get("Content-Length"))
+}
+
+func getWithQuery() {
+	// クエリ付きget method
+	values := url.Values{"query": {"hello world"}}
+	resp, _ := http.Get("http://localhost:18888" + "?" + values.Encode())
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println(string(body))
+}
+
+func main () {
+	simpleGet()
+	log.Println("=====================")
+	getWithQuery()
 }
